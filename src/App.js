42096d3a2dff1fc3162487527a9f6464334b9b2d
あかり <>
2024-02-24 16:18:48 +0900

    データ表示できた

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
   //Selector.jsxからお引越し。Top down Data Flowのため、親から情報を流さないいかん
   //propsで上位コンポーネントから下位コンポーネントへ渡したいものを包み込むふくさ
   const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState({
+    date : "",
+    newConfirmed : "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecovered: "",
+  });
+
 	function handleChangeCountry (e){
 		setCountry(e.target.value);
 	}
@@ -15,16 +23,25 @@ function App() {
 	function getCountryData() {
 		fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
 			.then(res => res.json())
-			.then(data =>  console.log(data))
+      .then(data => setCountryData({
+        date: data[data.length-1].Date,
+        newConfirmed : data[data.length-1].Confirmed - data[data.length-2].Confirmed,
+        totalConfirmed: data[data.length-1].Confirmed,
+        newRecovered: data[data.length-1].Recovered - data[data.length-2].Recovered,
+        totalRecovered: data[data.length-1].Recovered,
+      }))
+			// .then(data =>  console.log(data[data.length-1]))
 	}
 
   return (
     <>
+      {console.log(countryData)}
       <TopPage
         handleChangeCountry={handleChangeCountry}
         countriesJson={countriesJson}
-        setCountry={setCountry}
-        getCountryData={getCountryData} />
+        // setCountry={setCountry}
+        getCountryData={getCountryData}
+        countryData={countryData} />
     </>
   );
 }
